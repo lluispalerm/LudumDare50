ddcacd2c3676082497e9294f22da81bcdb660efe
lluispalerm <48449992+lluispalerm@users.noreply.github.com>
2022-04-03 11:05:45 +0200

    Barras barrar

--- Assets/Scripts/BarraBehaviour.cs
+++ Assets/Scripts/BarraBehaviour.cs
@@ -6,21 +6,32 @@ using UnityEngine;
 public class BarraBehaviour : MonoBehaviour
 {
     public float max = 1f;
+
     public float actual = 0.5f;
     public Image fillImage;
+
+    void Start(){
+        fillImage.fillAmount = actual;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //For demo
         if (Input.GetKeyDown("q")){
-            actual -= 0.05f;
-            Mathf.Clamp(actual, 0, 1);
-            fillImage.fillAmount = actual;
+            UpdateBarraStatus(-0.05f);
         }
         if (Input.GetKeyDown("e")){
-            actual += 0.05f;
-            Mathf.Clamp(actual, 0, 1);
-            fillImage.fillAmount = actual;
+            Debug.Log(PercentageActual());
+            UpdateBarraStatus(0.05f);
         }
     }
+
+    public void UpdateBarraStatus(float value = -0.05f){
+        actual += value;
+        actual = Mathf.Clamp(actual, 0, 1);
+        fillImage.fillAmount = actual;
+    }
+
+    public float PercentageActual() => actual * 100.0f;
 }
