7a25f048471b5d3e0828e83a2c4f8f464b7e86ef
lluispalerm <48449992+lluispalerm@users.noreply.github.com>
2022-04-04 00:13:20 +0200

    Player final

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,25 @@ public class PlayerController : MonoBehaviour
     public float deadZone;
     public Rigidbody2D rb;
 
+
+    //Ref
     public IntecteableObject currentObject;
+    public SpriteRenderer renderer;
+
+    //Movement
+    public Sprite[] sprites;
+    private bool moving = false;
+    [Header("Animation")]
+    public int iterator = 0;
+    public int limitIterator = 5;
+    public int mFrame = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         //controller = gameObject.GetComponent<CharacterController>();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        renderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,13 +39,35 @@ public class PlayerController : MonoBehaviour
 
         float hAxis = Input.GetAxis("Horizontal");
         if (Mathf.Abs(hAxis) > 0.3f){
+            moving = true;
             Move(hAxis);
         }
+        else{
+            if(moving){ 
+                renderer.sprite = sprites[0];
+                mFrame = 1;
+            }
+            moving = false;
+        }
+
+        if(moving){
+            renderer.sprite = sprites[mFrame];
+            if(iterator >= limitIterator){
+                mFrame++;
+                if(mFrame >= sprites.Length) mFrame = 1;
+                iterator = -1;
+            }
+            iterator++;
+        }
+
+        
     }
 
     public void Move(float hAxis){
         //controller.Move((new Vector3(speed,0,0) * hAxis) * Time.deltaTime);
         //rb.Move( * Time.deltaTime);
+        if(hAxis < 0) renderer.flipX = true;
+        else renderer.flipX = false;
         rb.MovePosition(rb.position + (new Vector2(speed,0) * hAxis) * Time.fixedDeltaTime);
     }
 
